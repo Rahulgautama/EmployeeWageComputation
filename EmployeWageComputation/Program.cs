541a8cb169aabcb44bfe5d3fa4b628f537b7e7ea
Rahulgautama <rahul8874950727@gmail.com>
2023-04-26 08:05:25 +0530

    Employee Wage computation program

---
+++ EmployeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+        }
+    }
+}
