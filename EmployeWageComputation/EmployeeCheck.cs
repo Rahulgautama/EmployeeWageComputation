4f1bd31375678689c21f2ef9d9e9e4593b2094c7
Rahulgautama <rahul8874950727@gmail.com>
2023-04-27 07:28:05 +0530

    Employee Wage Part

--- EmployeWageComputation/EmployeeCheck.cs
+++ EmployeWageComputation/EmployeeCheck.cs
@@ -10,15 +10,22 @@ namespace EmployeWageComputation
         {
             int EMP_RAT_PER_HOUR = 20;
             int Is_Full_Time = 1;
+            int IS_PART_Time = 2;
             int empHour = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck =random.Next(2);
+            int empCheck =random.Next(3);
             if (empCheck == Is_Full_Time)
             {
                 empHour = 8;
             }
-            else {
+            else if (empCheck == IS_PART_Time)
+            {
+                empHour = 4;
+            }
+            else 
+            {
+
                 empHour = 0;
             }
             empWage = empHour * EMP_RAT_PER_HOUR;
