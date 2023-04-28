ff3cde54baf89a7beb655bccc587c50c11247114
Rahulgautama <rahul8874950727@gmail.com>
2023-04-28 21:26:44 +0530

    Calculate Employee Wage a month 20 day 100 hours

--- EmployeWageComputation/Program.cs
+++ EmployeWageComputation/Program.cs
@@ -6,8 +6,20 @@ namespace EmployeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            //Console.WriteLine("Welcome To Employee Wage Computation Program");
             EmployeeCheck.EmployeeAttendance();
+
+            //int a = 10;
+            //int[] arr = new int[3];
+            //arr[0]=20;
+            //arr[1]=50;
+            //arr[2]=60;
+
+            //int[] arr1 = new int[] { 10, 20 };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
         }
     }
 }
